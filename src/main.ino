6b5caeaad75e5125889744411bd45cfb718b9039
andyp13 <andy@MacBook-Pro.local>
2016-09-22 10:56:27 -0600

    Bugfixes

--- src/main.ino
+++ src/main.ino
@@ -62,7 +62,7 @@ void loop() {
   //Check Time
   currentTime = millis();
 
-  //Class Loops is it working
+  //Class Loops 
   mainButton.loop();
   compass.loop();
   mainController.loop();    //TODO: Replace with correct controller here
